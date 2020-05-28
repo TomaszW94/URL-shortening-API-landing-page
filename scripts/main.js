42cd10dc403fd92261a545bfb65f4030141ece8a
TomaszW94 <tomek140194@gmail.com>
2020-05-28 21:16:10 +0200

    fix bugs

--- scripts/main.js
+++ scripts/main.js
@@ -13,10 +13,8 @@ btnMenu.addEventListener('click', function () {
     menu.classList.toggle('active');
 })
 
-
-// CREATE ELEMENTS UL/LI/BUTTON shortin-link
-
-const createElements = () => {
+const createElements = (e) => {
+    e.preventDefault();
     if ($input_shorten.value.toLowerCase().includes('http://') || $input_shorten.value.toLowerCase().includes('https://')) {
 
         //create element list ul
@@ -57,43 +55,49 @@ const createElements = () => {
         btn_shortlink.innerText = "Copy";
         //delete warning message
         $pWarning.innerText = "";
-
-        //function copyToClipBoard
-        function copyToClipboard(e) {
-            e.preventDefault();
-            const btnCopy = e.target;
-            const linkShortCopy = event.target.parentElement.querySelector('.new_link');
-            const textArea = document.createElement("textarea");
-            textArea.value = linkShortCopy.textContent;
-            document.body.appendChild(textArea);
-            textArea.select();
-            document.execCommand("copy");
-            textArea.remove();
-            btnCopy.classList.add('clicked-btn');
-            btnCopy.innerText = "Copied!";
-        }
-
         //Copy button
         btn_shortlink.addEventListener('click', copyToClipboard);
+        //axios POST
+        axiosPOST();
+    }
+}
 
-        //Axios method POST
-        axios.post('https://rel.ink/api/links/', {
-                "url": $input_shorten.value
-            })
-            .then(function (res) {
-                aShortenlink.innerText = `https://rel.ink/${res.data.hashid}`;
-                $input_shorten.value = '';
-                $input_shorten.classList.remove('invaild');
-            })
-            .catch(function (err) {
-                console.log(err);
-            });
+//Axios function POST
+const axiosPOST = () => {
+    axios.post('https://rel.ink/api/links/', {
+            "url": $input_shorten.value
+        })
+        .then(function (res) {
+            document.querySelector('.new_link').innerText = `https://rel.ink/${res.data.hashid}`;
+            $input_shorten.value = '';
+            $input_shorten.classList.remove('invaild');
+        })
+        .catch(function (err) {
+            console.log(err);
+        })
+};
 
+//function copyToClipBoard
+function copyToClipboard(e) {
+    e.preventDefault();
+    const btnCopy = e.target;
+    const linkShortCopy = event.target.parentElement.querySelector('.new_link');
+    const textArea = document.createElement("textarea");
+    textArea.value = linkShortCopy.textContent;
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand("copy");
+    textArea.remove();
+    btnCopy.classList.add('clicked-btn');
+    btnCopy.innerText = "Copied!";
+}
+
+$input_shorten.addEventListener('input', () => {
+    if ($input_shorten.value.toLowerCase().includes('http://') || $input_shorten.value.toLowerCase().includes('https://')) {
+        $pWarning.innerHTML = "";
+        $btn_shortenit.addEventListener('click', createElements);
     } else {
         $pWarning.innerHTML = "Please add a link (http:// or https://)";
         $input_shorten.classList.add('invaild');
     }
-}
-
-
-$btn_shortenit.addEventListener('click', createElements);
+});

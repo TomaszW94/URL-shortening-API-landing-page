a40aac0d777e66957673d1754d2de4e5b23e05e7
TomaszW94 <tomek140194@gmail.com>
2020-05-29 10:34:26 +0200

    added link validator

--- scripts/main.js
+++ scripts/main.js
@@ -4,8 +4,6 @@ const $btn_shortenit = document.querySelector('.btn-shortenit');
 const $pWarning = document.querySelector('.warning');
 
 
-
-
 //OPEN / CLOSE MENU
 const btnMenu = document.querySelector('.menu-btn');
 btnMenu.addEventListener('click', function () {
@@ -15,51 +13,57 @@ btnMenu.addEventListener('click', function () {
 
 const createElements = (e) => {
     e.preventDefault();
-    if ($input_shorten.value.toLowerCase().includes('http://') || $input_shorten.value.toLowerCase().includes('https://')) {
+    //create element list ul
+    const ulShortenlinks = document.createElement("ul");
 
-        //create element list ul
-        const ulShortenlinks = document.createElement("ul");
+    //where create this element
+    const reference = document.querySelector('.shorten-it');
+    reference.after(ulShortenlinks);
+    ulShortenlinks.id = "shorten-links";
+    ulShortenlinks.className = "shorten-links-list";
 
-        //where create this element
-        const reference = document.querySelector('.shorten-it');
-        reference.after(ulShortenlinks);
-        ulShortenlinks.id = "shorten-links";
-        ulShortenlinks.className = "shorten-links-list";
+    //create element li inside ul
+    const liShorten = document.createElement("li");
+    ulShortenlinks.appendChild(liShorten);
+    liShorten.className = "card-link";
 
-        //create element li inside ul
-        const liShorten = document.createElement("li");
-        ulShortenlinks.appendChild(liShorten);
-        liShorten.className = "card-link";
+    //create div inside li
+    const divShortenBase = document.createElement("div");
+    const divShortenLink = document.createElement("div");
+    liShorten.appendChild(divShortenBase);
+    liShorten.appendChild(divShortenLink);
+    divShortenBase.className = "base-link";
+    divShortenLink.className = "shorten-link";
 
-        //create div inside li
-        const divShortenBase = document.createElement("div");
-        const divShortenLink = document.createElement("div");
-        liShorten.appendChild(divShortenBase);
-        liShorten.appendChild(divShortenLink);
-        divShortenBase.className = "base-link";
-        divShortenLink.className = "shorten-link";
+    //create element p and button inside div
+    const aBaselink = document.createElement("a");
+    divShortenBase.appendChild(aBaselink);
+    const aShortenlink = document.createElement("a");
+    divShortenLink.appendChild(aShortenlink);
+    aBaselink.setAttribute("href", $input_shorten.value.toLowerCase());
+    aBaselink.innerText = $input_shorten.value.toLowerCase();
+    aShortenlink.setAttribute("href", $input_shorten.value.toLowerCase());
+    aShortenlink.innerText = $input_shorten.value.toLowerCase();
+    aShortenlink.className = "new_link";
+    const btn_shortlink = document.createElement("button");
+    btn_shortlink.className = "btn_shortLink";
+    divShortenLink.appendChild(btn_shortlink);
+    btn_shortlink.innerText = "Copy";
+    //delete warning message
+    $pWarning.innerText = "";
 
-        //create element p and button inside div
-        const aBaselink = document.createElement("a");
-        divShortenBase.appendChild(aBaselink);
-        const aShortenlink = document.createElement("a");
-        divShortenLink.appendChild(aShortenlink);
-        aBaselink.setAttribute("href", $input_shorten.value.toLowerCase());
-        aBaselink.innerText = $input_shorten.value.toLowerCase();
-        aShortenlink.setAttribute("href", $input_shorten.value.toLowerCase());
-        aShortenlink.innerText = $input_shorten.value.toLowerCase();
-        aShortenlink.className = "new_link";
-        const btn_shortlink = document.createElement("button");
-        btn_shortlink.className = "btn_shortLink";
-        divShortenLink.appendChild(btn_shortlink);
-        btn_shortlink.innerText = "Copy";
-        //delete warning message
-        $pWarning.innerText = "";
-        //Copy button
-        btn_shortlink.addEventListener('click', copyToClipboard);
-        //axios POST
-        axiosPOST();
-    }
+    //axios POST
+    axiosPOST();
+
+    //remove input value
+    $input_shorten.value = "";
+
+    //Copy button
+    btn_shortlink.addEventListener('click', copyToClipboard);
+
+    //remove event listener
+    $btn_shortenit.removeEventListener('click', createElements);
+
 }
 
 //Axios function POST
@@ -93,11 +97,23 @@ function copyToClipboard(e) {
 }
 
 $input_shorten.addEventListener('input', () => {
-    if ($input_shorten.value.toLowerCase().includes('http://') || $input_shorten.value.toLowerCase().includes('https://')) {
-        $pWarning.innerHTML = "";
-        $btn_shortenit.addEventListener('click', createElements);
+    $btn_shortenit.removeEventListener('click', createElements);
+    const regexpHttp = new RegExp("^(http|https):\/\/");
+    const regexpLinkV = new RegExp('((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+        '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+        '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+        '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator)
+    if (regexpHttp.test($input_shorten.value.toLowerCase())) {
+        if (regexpLinkV.test($input_shorten.value.toLowerCase())) {
+            $btn_shortenit.addEventListener('click', createElements);
+            $pWarning.textContent = "";
+        } else {
+            $pWarning.textContent = "Enter the correct link";
+        }
     } else {
-        $pWarning.innerHTML = "Please add a link (http:// or https://)";
+        $pWarning.textContent = "Please add a link (http:// or https://)";
         $input_shorten.classList.add('invaild');
     }
+
 });
